6216b4ab358421ebe3a690de34679a157da9f87b
misaellopes01 <mecl.ely@gmail.com>
2024-11-17 23:56:36 +0100

    create account route

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,8 +17,30 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    () => {
-      return 'Created'
+    async (request, reply) => {
+      const { email, name, password } = request.body
+
+      const userExists = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (userExists) {
+        return reply
+          .status(400)
+          .send({ message: 'user with same email already exists' })
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      return reply.status(201).send()
     },
   )
 }
